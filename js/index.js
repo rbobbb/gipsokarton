14a0bf9e1dd28a180d18ee2553fa351a36c1a6da
Roman <roman.bobyk@gmail.com>
2019-03-13 14:18:11 +0000

    css calc

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const range <a href="https://www.jqueryscript.net/slider/">Slider</a> = function(){
+const slider = $('.range-slider'),
+    range = $('.range-slider__range'),
+    value = $('.range-slider__value');
+  
+slider.each(function(){
+
+  value.each(function(){
+    var value = $(this).prev().attr('value');
+    $(this).html(value);
+  });
+
+  range.on('input', function(){
+    $(this).next(value).html(this.value);
+  });
+});
+};
+
+rangeSlider();
